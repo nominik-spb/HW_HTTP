c88d31c3b21c46c46c1be841809b88e8c879b654
nominik <mike.nogai.spb@mail.ru>
2025-10-19 23:25:10 +0300

    change

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,17 +1,53 @@
 package ru.netology;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+    public static ObjectMapper mapper = new ObjectMapper();
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(RequestConfig.custom()
+                        .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                        .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                        .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                        .build())
+                .build();
+
+        HttpGet request = new HttpGet(URL);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+
+        CloseableHttpResponse response = httpClient.execute(request);
+        try {
+        Arrays.stream(response.getAllHeaders()).forEach(System.out::println);
+
+        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
+        System.out.println(body);
+
+
+            List<Post> posts = mapper.readValue(response.getEntity().getContent(), new TypeReference<List<Post>>() {
+            });
+            posts.stream()
+                    .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                    .forEach(System.out::println);
+        } finally {
+            response.close;
         }
     }
 }
--- src/main/java/ru/netology/Post.java
+++ src/main/java/ru/netology/Post.java
@@ -1,4 +1,46 @@
 package ru.netology;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.OptionalInt;
+
 public class Post {
+
+    private final int userId;
+    private final int id;
+    private final String title;
+    private final String body;
+    private final Integer upvotes;
+    public Post(
+            @JsonProperty("userId") int userId,
+            @JsonProperty("id") int id,
+            @JsonProperty("title") String title,
+            @JsonProperty("body") String body,
+            @JsonProperty("upvotes") Integer upvotes
+    ) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.upvotes = upvotes;
+    }
+    public int getUserId() {
+        return userId;
+    }
+
+    public Integer getUpvotes() {
+        return upvotes;
+    }
+
+    @Override
+    public String toString() {
+        return "Post" +
+                "\n userId=" + userId+
+                "\n id=" + id +
+                "\n title=" + title +
+                "\n body=" + body +
+                "\n upvotes=" + upvotes;
+    }
+
+
 }
